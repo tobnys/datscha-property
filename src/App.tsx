b49e767258d30184e7c5b08a524a8ae8ff6c4df7
tobnys <tobnystrom@gmail.com>
2021-03-10 14:12:38 +0100

    Add several packages and added basic page for login

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import styled, { createGlobalStyle } from 'styled-components'
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <GlobalStyle />
+      <LoginBox />
     </div>
   );
 }
 
-export default App;
+const LoginBox = styled.div`
+  width: 300px;
+  background-color: #242b33;
+  padding: 45px;
+  position: absolute;
+  top: 45%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+`
+
+const GlobalStyle = createGlobalStyle`
+  html {
+    height: 100%;
+    width: 100%;
+    margin: 0;
+    padding: 0;
+    background: rgb(6,10,11);
+    background: linear-gradient(360deg, rgba(6,10,11,1) 25%, rgba(36,43,52,1) 100%);
+    font-family: 'Manrope', sans-serif;
+  }
+`
